ee59ecf0aca17187ceabde8a4ed497f9263b8117
Uğurhan Bilici <ubilici94@gmail.com>
2016-11-13 14:00:28 +0300

    add test scene add unit component add input manager add basic movement controls

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InputManager : MonoBehaviour
+{
+    public List<Unit> selectedUnits;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Transform selectedTransform = DetectObject().transform;
+            if(selectedTransform != null)
+            {
+                if (selectedTransform.GetComponent<Unit>())
+                {
+                    // Multiselect
+                    if (Input.GetKey(KeyCode.LeftControl))
+                    {
+                        SelectUnit(selectedTransform.GetComponent<Unit>());
+                    }
+                    // Deselect rest
+                    else
+                    {
+                        DeselectAllUnits();
+                        SelectUnit(selectedTransform.GetComponent<Unit>());
+                    }
+                }
+                else
+                {
+                    DeselectAllUnits();
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if(selectedUnits.Count != 0)
+                {
+                    foreach (Unit unit in selectedUnits)
+                    {
+                        unit.Move(hit.point);
+                    }
+                }
+            }
+        }
+    }
+
+    RaycastHit DetectObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        Physics.Raycast(ray, out hit, 100);
+
+        return hit;
+    }
+
+    void DeselectAllUnits()
+    {
+        foreach (Unit unit in selectedUnits)
+        {
+            unit.Disable();
+        }
+        selectedUnits.Clear();
+    }
+
+    void SelectUnit(Unit selectedUnit)
+    {
+        if (selectedUnit.unitFaction == UnitFaction.Ally)
+        {
+            selectedUnits.Add(selectedUnit);
+            selectedUnit.Enable();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour
+{
+    public UnitState unitState;
+    public UnitAttackType unitAttackType;
+    public UnitFaction unitFaction;
+
+    public float unitMoveSpeed;
+    public float unitAttackSpeed;
+    public float unitAttackDamage;
+
+    private bool moveSequence;
+    private Vector3 targetPosition;
+
+    void FixedUpdate()
+    {
+        if (moveSequence)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * unitMoveSpeed);
+
+            if(transform.position == targetPosition)
+            {
+                unitState = UnitState.Idle;
+                moveSequence = false;
+            }
+        }
+    }
+
+    public void Move(Vector3 _targetPos)
+    {
+        targetPosition = _targetPos + new Vector3(0, transform.position.y);
+        moveSequence = true;
+        unitState = UnitState.Move;
+    }
+
+    void Attack()
+    {
+
+    }
+
+    public void Enable()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.green;
+    }
+
+    public void Disable()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.black;
+    }
+}
+
+public enum UnitState
+{
+    Idle,
+    Move,
+    Attack
+}
+
+public enum UnitAttackType
+{
+    Melee,
+    Ranged
+}
+
+public enum UnitFaction
+{
+    Ally,
+    Enemy
+}
